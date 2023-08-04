a81ec1c7aa2d1d6d4fd4800de9668085be74a835
Alejandro Depetris <aledepetris@outlook.com>
2023-08-04 11:45:53 -0300

    Primeras validaciones y controlFormas reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent implements OnInit {
+export class BasicPageComponent {
 
-  constructor() { }
+/*   public myForm: FormGroup = new FormGroup({
+    // Valor por defecto, validaciones sincronas y luego asincronas
+    name: new FormControl('', [], []),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+  }) */
 
-  ngOnInit(): void {
+  // Otra forma con un builder
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]]
+  })
+
+  constructor ( private fb: FormBuilder ) {}
+
+  onSave(): void {
+
+    if ( this.myForm.invalid ) return;
+
+    console.log(this.myForm.value);
+
   }
 
 }
