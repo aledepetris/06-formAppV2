13ca9a935dd65826293c6af6deb73bfb7e0f2d01
Alejandro Depetris <aledepetris@outlook.com>
2023-08-10 16:26:41 -0300

    Comienzo de forms dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: [
   ]
 })
-export class DynamicPageComponent implements OnInit {
+export class DynamicPageComponent {
 
-  constructor() { }
+  //public myForm2 = new FormGroup({
+  //  favoriteGames: new FormArray([])
+  //})
 
-  ngOnInit(): void {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      [ 'Dota 2', Validators.required ],
+      [ 'Cs Go', Validators.required ]
+    ])
+  })
+  constructor( private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
   }
+
+  onSubmit(): void {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+
+  }
+
+
 
 }
