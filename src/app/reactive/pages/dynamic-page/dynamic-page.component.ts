ca4242e634cca861ea2bfb7156957840f6041beb
Alejandro Depetris <aledepetris@outlook.com>
2023-08-11 15:56:03 -0300

    Eliminacion de elementos del FormArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -57,8 +57,9 @@ export class DynamicPageComponent {
 
   }
 
-
-
+  onDeleteFavorite( index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
 
   onSubmit(): void {
     if ( this.myForm.invalid ) {
